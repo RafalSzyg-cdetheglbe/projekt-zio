c4d8ec96d564c6eec40ecfa804864adf2c0a4922
MichalKupczyk1 <72615252+MichalKupczyk1@users.noreply.github.com>
2024-03-17 17:39:31 +0100

    Dodanie klasy UserDTO

---
+++ WebApi/WebApi/Models/DTO/UserDTO.cs
@@ -0,0 +1,20 @@
+﻿using WebApi.Models.DbEntities.UserEntities;
+
+namespace WebApi.Models.DTO
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public UserType UserType { get; set; }
+        public bool IsActive { get; set; }
+        public UserDTO(User user)
+        {
+            Id = user.Id;
+            Name = user.Name;
+            UserType = user.UserType;
+            IsActive = user.IsActive;
+        }
+
+    }
+}
