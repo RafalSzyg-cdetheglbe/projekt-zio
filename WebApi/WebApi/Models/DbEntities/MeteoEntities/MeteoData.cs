50e4def5e9ddb0796e80f71ab66b5d79bfaeb1de
MichalKupczyk1 <72615252+MichalKupczyk1@users.noreply.github.com>
2024-03-25 09:15:16 +0100

    Dodanie klas opisujących stację meteo oraz dane na niej zapisywane

---
+++ WebApi/WebApi/Models/DbEntities/MeteoEntities/MeteoData.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApi.Models.DbEntities.AuditAndContext;
+
+namespace WebApi.Models.DbEntities.MeteoEntities
+{
+    public class MeteoData
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double NumericValue { get; set; }
+        public string StringValue { get; set; }
+        public string Description { get; set; }
+        public string Unit { get; set; }
+        public BaseAuditData AuditData { get; set; }
+    }
+}
---
+++ WebApi/WebApi/Models/DbEntities/MeteoEntities/MeteoStation.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using WebApi.Models.DbEntities.UserEntities;
+
+namespace WebApi.Models.DbEntities.MeteoEntities
+{
+    public class MeteoStation
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public User Creator { get; set; }
+        public int CreatorId { get; set; }
+        public IEnumerable<MeteoData>? MeteoData { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+    }
+}
