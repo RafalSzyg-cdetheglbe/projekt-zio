7701c65d4bc4b69ce79e46897cfaa435d618da6e
MichalKupczyk1 <72615252+MichalKupczyk1@users.noreply.github.com>
2024-04-05 16:17:55 +0200

    Dodanie DTO koordynatów oraz kontrolera dla stacji meteo

---
+++ WebApi/WebApi/Controllers/MeteoStationsController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApi.Models.DTO;
+using WebApi.Services.Interfaces;
+
+namespace WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MeteoStationsController : ControllerBase
+    {
+        private readonly IMeteoStationInterface _meteoStationService;
+
+        public MeteoStationsController(IMeteoStationInterface meteoStationService)
+        {
+            _meteoStationService = meteoStationService;
+        }
+
+        [HttpPost]
+        public ActionResult<int> AddMeteoStation(MeteoStationDTO dto)
+        {
+            var stationId = _meteoStationService.AddMeteoStation(dto);
+            return stationId >= 0 ? (ActionResult<int>)Ok(stationId) : BadRequest();
+        }
+
+        [HttpPost("{id}/data")]
+        public IActionResult AddMeteoData(int id, MeteoDataDTO dto)
+        {
+            _meteoStationService.AddNewMeteoData(id, dto);
+            return Ok();
+        }
+
+        [HttpPut("{id}/coordinates")]
+        public IActionResult UpdateMeteoStationCoordinates(int id, [FromBody] CoordinatesDTO coordinates)
+        {
+            _meteoStationService.ChangeMeteoStationCoordinates(id, coordinates.Latitude, coordinates.Longitude);
+            return Ok();
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult UpdateMeteoStation(int id, MeteoStationDTO dto)
+        {
+            _meteoStationService.UpdateMeteoStation(dto);
+            return Ok();
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult DeleteMeteoStation(int id)
+        {
+            var success = _meteoStationService.DeleteMeteoStation(id);
+            return success ? (IActionResult)Ok() : NotFound();
+        }
+    }
+}
