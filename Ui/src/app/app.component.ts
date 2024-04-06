c889924ba26e6a6a1945e08cb69a090e6581e2c2
Slawinek <slawzgl413@student.polsl.pl>
2024-04-06 18:10:07 +0200

    add view with map

--- Ui/src/app/app.component.ts
+++ Ui/src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {ButtonModule} from 'primeng/button'
+import { ToolbarModule } from 'primeng/toolbar';
+import { AvatarModule } from 'primeng/avatar';
+import {AngularOpenlayersModule} from "ng-openlayers";
+import { SplitterModule } from 'primeng/splitter';
+
+
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, ButtonModule, ToolbarModule, AvatarModule, AngularOpenlayersModule, SplitterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
